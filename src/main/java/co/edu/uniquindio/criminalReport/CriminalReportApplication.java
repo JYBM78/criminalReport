e9c3f3dcd0ee5bf40ffa0c36afd8fb2a640c09d3
José Yovany Beltrán Matallana <101234042+JYBM78@users.noreply.github.com>
2025-02-19 15:19:53 -0500

    first commit

---
+++ src/main/java/co/edu/uniquindio/criminalReport/CriminalReportApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.criminalReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriminalReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriminalReportApplication.class, args);
+
+    }
+}
